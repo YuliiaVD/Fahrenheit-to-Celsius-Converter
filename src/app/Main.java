05a9da3696d43f14af68011194968f84d45c4775
Yuliia Deineka <yu.deyneka@gmail.com>
2024-06-12 21:26:42 +0200

    Add Celsius to Fahrenheit converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,12 +6,18 @@ public class Main {
         System.out.println("My Fahrenheit to Celsius app.");
 
         double fahr = 100;
+        double cels = 38;
         double celsius = konvFahrToCel(fahr);
+        double fahren = konvCelsToFahr(cels);
 
-        System.out.println("Result is: " + celsius + "°C Celsius");
+        System.out.println("Result is: " + celsius + "°C Celsius and " + fahren + "°F Fahrenheit");
     }
 
     public static double konvFahrToCel (double fahr) {
         return (5.0/9.0) * (fahr - 32);
+    }
+
+    public static double konvCelsToFahr (double cels) {
+        return ((cels * 9)/5)+32;
     }
 }
